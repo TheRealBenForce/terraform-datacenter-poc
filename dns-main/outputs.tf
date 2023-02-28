d0a8aef5d84c464e17bba7322dda6e88fa2d3193
theRealBenForce <TheRealBenForce@gmail.com>
2023-02-28 13:59:37 -0700

    outputs

--- dns-main/outputs.tf
+++ dns-main/outputs.tf
@@ -1,9 +1,9 @@
 output "hosted_zone_id" {
   description = "Hosted zone ID"
-  value       = data.aws_route53_zone.terraform_subzone.zone_id
+  value       = terraform_subzone.zone_id
 }
 
 output "tf_subzone_name" {
   description = "Subzone where all teraform infrastructure created by pipeline will go"
-  value       = data.aws_route53_zone.terraform_subzone.name
+  value       = terraform_subzone.name
 }
